5a0f0cf80a6e82807c30f76825fe95952eb83236
ABDEL ILLAH HASSAINE <abdel.illah.x.hassaine.-nd@disney.com>
2021-01-06 21:52:51 +0100

    feat(cart): model

---
+++ src/models/cart/index.js
@@ -0,0 +1,36 @@
+
+class Cart {
+
+  /**
+   * 
+   * @param {String} id
+   */
+  static add (id) {
+    return new Promise((res, rej) => {
+      res()
+    })
+  }
+
+  /**
+   * 
+   * @param {String} id
+   */
+  static remove (id) {
+    return new Promise((res, rej) => {
+      res()
+    })
+  }
+
+  /**
+   * 
+   * @param {String} id
+   * @param {Number} amount
+   */
+  static update (id, amount) {
+    return new Promise((res, rej) => {
+      res()
+    })
+  }
+}
+
+module.exports = Cart
